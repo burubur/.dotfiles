ca7631a864a62f1b69f7c1305157c7a6f2cea735
Burhan Mubarok <burhanmubarok@gmail.com>
2020-06-07 00:29:50 +0700

    install neovim

--- install.sh
+++ install.sh
@@ -9,5 +9,24 @@ source ~/.dotfiles/.env
 source ~/.dotfiles/.inputrc
 source ~/.dotfiles/.alias
 tmux source ~/.tmux.conf
+
+echo "installing software..."
+
+#is_exist=$(which nvim | wc -l)
+#if [ ${is_exist} -ne 1 ]; then \
+#        echo "nvim is not installed"; \
+#	echo "installing neovim..." \
+#	curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz; \
+#	tar xzf nvim-macos.tar.gz; \
+#else \
+#        echo "nvim is installed"; \
+#fi;
+
+
+echo "installing neovim python module"
+pip3 install --user neovim
+
+echo "software installation done."
+
 echo ""
 echo "installed."
