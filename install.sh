4d82d77d15e2d8e379a76c8319474366954a1583
Burhan Mubarok <burhanmubarok@gmail.com>
2022-01-18 23:53:48 +0700

    add os check

--- install.sh
+++ install.sh
@@ -1,12 +1,25 @@
+# FOR SAFETY PURPOSE, PLEASE RUN THIS INSTALLATION 1 BY 1 BY REMARK AND UNREMARK THEM LINE BY LINE :)
 echo "installing software...\n"
 
+if [[ $OSTYPE != 'darwin'* ]]; then
+  echo "unsupported OS"
+  exit 0
+fi
+
+# Brew
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Iterm
+# Zsh
+# Git
+
 # Go
-$GOPATH/bin/g install latest
+# $GOPATH/bin/g install latest
 
 # Node & NPM
-test -e /Users/burhan.mubarok/n/bin/node && echo "installed node" || $(n latest && n lts)
+# test -e /Users/burhan.mubarok/n/bin/node && echo "installed node" || $(n latest && n lts)
 
 # Yarn
-test -e $HOME/.yarn/bin/yarn && echo "installed yarn" || curl -o- -L https://yarnpkg.com/install.sh | bash
+# test -e $HOME/.yarn/bin/yarn && echo "installed yarn" || curl -o- -L https://yarnpkg.com/install.sh | bash
 
 echo "\ninstallation done."
