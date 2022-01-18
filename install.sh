8373d7be0c6fd9dc23730e157b087959f01cb995
Burhan Mubarok <burhanmubarok@gmail.com>
2022-01-19 00:35:07 +0700

    modify installation line by line

--- install.sh
+++ install.sh
@@ -6,20 +6,35 @@ if [[ $OSTYPE != 'darwin'* ]]; then
   exit 0
 fi
 
-# Brew
+# BREW
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$USER/.zprofile
+# eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# Iterm
-# Zsh
-# Git
+# ITERM
+# brew install iterm2
 
-# Go
-# $GOPATH/bin/g install latest
+# ZSH
+# brew install zsh
+
+# GIT
+# brew install git
+
+# G - Golang package manager
+# curl -sSL https://git.io/g-install | sh -s
+
+# GO
+# g install 1.17
+# g install latest
+
+# N - NodeJS package manager
+# test -e /Users/$USER/n && echo "installed n package manager for NodeJS" || brew install n
 
 # Node & NPM
-# test -e /Users/burhan.mubarok/n/bin/node && echo "installed node" || $(n latest && n lts)
+# test -e /Users/$USER/n/bin/node && echo "installed node" || N_PREFIX=/Users/$USER n latest && n lts
 
 # Yarn
-# test -e $HOME/.yarn/bin/yarn && echo "installed yarn" || curl -o- -L https://yarnpkg.com/install.sh | bash
+# test -e /opt/homebrew/bin/yarn && echo "installed yarn" || brew install yarn
+# curl -o- -L https://yarnpkg.com/install.sh | bash
 
 echo "\ninstallation done."
